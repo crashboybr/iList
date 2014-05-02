6963e41e1852940acc7aa17a7b702d7adbb32dbf
Bernardo Alves <bernardo.d.alves@gmail.com>
2014-05-01 23:21:34 -0300

    files

--- changes.sql
+++ changes.sql
@@ -1,1 +1,7 @@
 ALTER TABLE ads ADD city_slug VARCHAR(100) DEFAULT NULL
+ALTER TABLE ads CHANGE price price VARCHAR(100) NOT NULL
+ALTER TABLE ads CHANGE price price DOUBLE PRECISION NOT NULL
+ALTER TABLE ads CHANGE price price VARCHAR(100) NOT NULL
+Nothing to update - your database is already in sync with the current entity metadata.
+ALTER TABLE ads CHANGE price price DOUBLE PRECISION NOT NULL
+ALTER TABLE ads CHANGE price price DOUBLE PRECISION NOT NULL
